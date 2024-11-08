e917ccbb31ec85f087065b60ea2684880269645a
Aleu79 <aleeaguero79@gmail.com>
2024-11-08 17:41:05 -0300

    Views Home

--- App/views.py
+++ App/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render,get_object_or_404,redirect
+from django.shortcuts import render
 from .forms import *
 from .models import *
-from django.contrib.auth import logout
-from django.contrib.auth.decorators import login_required, permission_required
-from django.db.models import Q
+
 
 # Create your views here.
 
+def Home(request):
+    buscar=Book.objects.all().order_by('book_id')[:3]
+    data={
+        'forms':buscar
+    }
+    return render (request,'index.html',data)
