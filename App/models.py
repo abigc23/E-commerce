2d80ea4d8ba88ef0ab0e4a89a91628e009fe45ac
Aleu79 <aleeaguero79@gmail.com>
2024-11-08 17:36:19 -0300

    Models

--- App/models.py
+++ App/models.py
@@ -1,3 +1,95 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here. 
+class Author(models.Model):
+    author_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    bio = models.TextField()
+    birth_date = models.DateField()
+    nationality = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Genre(models.Model):
+    genre_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    book_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    publication_date = models.DateField()
+    isbn = models.CharField(max_length=13, unique=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    cover_image = models.CharField(max_length=255)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class BookGenre(models.Model):
+    bookgenre_id = models.AutoField(primary_key=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('book', 'genre')
+
+    def __str__(self):
+        return f"{self.book.title} - {self.genre.name}"
+
+class Customer(models.Model):
+    customer_id = models.AutoField(primary_key=True)
+    phone = models.CharField(max_length=20)
+    address = models.TextField()
+    city = models.CharField(max_length=50)
+    postal_code = models.CharField(max_length=20)
+    country = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.city}, {self.country}'
+
+class Order(models.Model):
+    order_id = models.AutoField(primary_key=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    order_date = models.DateTimeField()
+    status = models.CharField(max_length=50)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    total_paid = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Order #{self.order_id} - {self.customer}"
+
+class OrderItem(models.Model):
+    orderitem_id = models.AutoField(primary_key=True)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.book.title} - Quantity: {self.quantity}"
+
+class CartItem(models.Model):
+    cartitem_id = models.AutoField(primary_key=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.customer} - {self.book.title} - Quantity: {self.quantity}"
+
+class Payment(models.Model):
+    payment_id = models.AutoField(primary_key=True)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    payment_date = models.DateTimeField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_method = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"Payment for Order #{self.order.order_id} - Amount: {self.amount}"
