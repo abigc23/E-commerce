b8048d823daad9f6b319e1f7bfe5220dd08256e5
abigc23 <rociogutierrezcao@gmail.com>
2024-11-08 17:49:41 -0300

    Forms

--- App/forms.py
+++ App/forms.py
@@ -1,2 +1,7 @@
 from django import forms
 from .models import *
+
+class NewBook(forms.ModelForm):
+    class Meta:
+        model=Book
+        fields='__all__'
--- App/views.py
+++ App/views.py
@@ -12,3 +12,5 @@ def Home(request):
     }
     return render (request,'index.html',data)
 
+
+
